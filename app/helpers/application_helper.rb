85e672d6c8005cf6ca884aadf032124ff145f320
Chris Eppstein <chris@eppsteins.net>
2008-10-02 18:06:33 -0700

    Give every page a semantic class and id.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,15 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def body_attributes
+    {:class => body_class, :id => body_id}
+  end
+
+  def body_class
+    @controller.controller_name.dasherize
+  end
+
+  def body_id
+    "#{@controller.controller_name.dasherize}-#{@controller.action_name.dasherize}"
+  end
+
 end
