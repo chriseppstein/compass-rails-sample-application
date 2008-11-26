bea6dac69bd07f1e91f759245b0265623f0c53e5
Chris Eppstein <chris@eppsteins.net>
2008-11-25 22:25:28 -0800

    Updated to reflect recent changes to compass.

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -4,5 +4,5 @@ Sass::Plugin.options[:template_location] = {
   "#{RAILS_ROOT}/app/stylesheets" => "#{RAILS_ROOT}/public/stylesheets/compiled"
 }
 Compass::Frameworks::ALL.each do |framework|
-  Sass::Plugin.options[:template_location][framework.stylesheets_directory] = "#{RAILS_ROOT}/public/stylesheets/public/stylesheets/compiled/#{framework.name}"
+  Sass::Plugin.options[:template_location][framework.stylesheets_directory] = "#{RAILS_ROOT}/public/stylesheets/compiled/#{framework.name}"
 end
