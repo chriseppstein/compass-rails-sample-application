fedda9117fc50a4aaa735cd31ac2d4165286bcd3
Andreas Haller <andreashaller@gmail.com>
2009-05-30 17:05:30 +0200

    detect rubygems with Gem::RubyGemsVersion

--- config/boot.rb
+++ config/boot.rb
@@ -83,8 +83,8 @@ module Rails
       def load_rubygems
         require 'rubygems'
 
-        unless rubygems_version >= '0.9.4'
-          $stderr.puts %(Rails requires RubyGems >= 0.9.4 (you have #{rubygems_version}). Please `gem update --system` and try again.)
+        unless Gem::RubyGemsVersion >= '0.9.4'
+          $stderr.puts %(Rails requires RubyGems >= 0.9.4 (you have #{Gem::RubyGemsVersion}). Please `gem update --system` and try again.)
           exit 1
         end
 
