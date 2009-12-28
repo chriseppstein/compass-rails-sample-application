17174d4319a0142c85fe7080b025eb4387cd9d91
Chris Eppstein <chris@eppsteins.net>
2009-12-28 11:16:31 -0800

    Update configuration to match the initial output from compass v0.10.

---
+++ config/compass.rb
@@ -0,0 +1,9 @@
+# Require any additional compass plugins here.
+project_type = :rails
+project_path = RAILS_ROOT if defined?(RAILS_ROOT)
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "public/stylesheets/compiled"
+sass_dir = "app/stylesheets"
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,8 +1,6 @@
 require 'compass'
-# If you have any compass plugins, require them here.
-Compass.configuration do |config|
-  config.project_path = RAILS_ROOT
-  config.sass_dir = "app/stylesheets"
-  config.css_dir = "public/stylesheets/compiled"
-end
-Compass.configure_sass_plugin!
+  # If you have any compass plugins, require them here.
+  Compass.configuration.parse(File.join(RAILS_ROOT, "config", "compass.rb"))
+  Compass.configuration.environment = RAILS_ENV.to_sym
+  Compass.configure_sass_plugin!
+  
